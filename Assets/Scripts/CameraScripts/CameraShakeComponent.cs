16c1a1809a35bc1e31061547f5d2a3c9dd139578
Ryan Andersen <ryan.andersen05@gmail.com>
2020-11-28 01:27:12 -0500

    Adding camera scripts to project

---
+++ Assets/Scripts/CameraScripts/CameraShakeComponent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShakeComponent : MonoBehaviour
+{
+    public float TimeRemainingForCameraShake;
+    public float CameraShakeIntensity;
+
+
+
+
+    private IEnumerator CameraShakeCoroutine()
+    {
+        yield return null;
+    }
+}
---
+++ Assets/Scripts/CameraScripts/MainCameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraFollow : MonoBehaviour
+{
+    [Range(0f, 1f)]
+    public float CameraFollowSpeed = .5f;
+
+    private Vector2 CameraOffsetFromTarget;
+    private Transform TargetTransform;
+    private Camera CameraComponent;
+
+    #region monobehaviour methods
+    private void Awake()
+    {
+        CameraComponent = GetComponentInChildren<Camera>();
+    }
+    #endregion monobehaviour methods
+
+    public void SetCameraFollowTarget(Transform TargetTransform)
+    {
+        this.TargetTransform = TargetTransform;
+    }
+
+    public void SetCameraOffsetFromTarget(Vector2 CameraOffsetFromTarget)
+    {
+        this.CameraOffsetFromTarget = CameraOffsetFromTarget;
+    }
+}
