e87db6819632fe0b4836b5d32cec6b11f62bfbbf
Ryan Andersen <ryan.andersen05@gmail.com>
2021-01-30 20:47:04 -0500

    Rooms now have bounds for camera

--- Assets/Scripts/CameraScripts/MainCameraFollow.cs
+++ Assets/Scripts/CameraScripts/MainCameraFollow.cs
@@ -32,8 +32,8 @@ public class MainCameraFollow : MonoBehaviour
     {
         Vector3 TargetPosition = TargetTransform.position + CameraOffsetFromTarget;
 
-
         this.transform.position = Vector3.Lerp(transform.position, TargetPosition, EHTime.DeltaTime * CameraFollowSpeed);
+        UpdateCameraBounds();
     }
     #endregion monobehaviour methods
 
@@ -50,5 +50,37 @@ public class MainCameraFollow : MonoBehaviour
     public void FocusCameraImmediate()
     {
         this.transform.position = TargetTransform.position + CameraOffsetFromTarget;
+    }
+
+    private void UpdateCameraBounds()
+    {
+        // A lot of this stuff can probably be calculated at the beginning to avoid doind this every
+        // update
+        Vector2 MinBounds, MaxBounds;
+        float OrthoSize = CameraComponent.orthographicSize;
+        Vector2 CameraSize = new Vector2(OrthoSize * Screen.width / Screen.height, OrthoSize);
+        MinBounds = new Vector2(transform.position.x, transform.position.y) - Vector2.one * CameraSize / 2f;
+        MaxBounds = MinBounds + CameraSize;
+        Vector2 RoomMin = BaseGameOverseer.Instance.CurrentlyLoadedRoom.GetMinRoomBounds();
+        Vector2 RoomMax = BaseGameOverseer.Instance.CurrentlyLoadedRoom.GetMaxRoomBounds();
+
+        if (MinBounds.x < RoomMin.x)
+        {
+            transform.position += Vector3.right * (RoomMin.x - MinBounds.x);
+        }
+        else if (MaxBounds.x > RoomMax.x)
+        {
+            transform.position += Vector3.left * (MaxBounds.x - RoomMax.x);
+        }
+
+        if (MinBounds.y < RoomMin.y)
+        {
+            transform.position += Vector3.up * (RoomMin.y - MinBounds.y);
+        }
+        else if (MaxBounds.y > RoomMax.y)
+        {
+            transform.position += Vector3.down * (MaxBounds.y - RoomMax.y);
+        }
+        
     }
 }
